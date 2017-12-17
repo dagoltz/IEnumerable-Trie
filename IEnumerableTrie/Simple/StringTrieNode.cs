3f5353afc5b8f83b9a9492e88451a6925aeeaab3
degoltz <daniel@goltz.info>
2017-12-17 21:25:54 +0200

    add missing StringTrieNode

---
+++ IEnumerableTrie/Simple/StringTrieNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableTrie.Simple
+{
+    public class StringTrieNode : IHasStringKeys
+    {
+        public string[] Keys { get; private set; }
+
+        public StringTrieNode(string key)
+        {
+            Keys = new []{key};
+        }
+
+        public static implicit operator StringTrieNode(string str)
+        {
+            return new StringTrieNode(str);
+        }
+
+        public static implicit operator string(StringTrieNode node)
+        {
+            return node.Keys.First();
+        }
+
+        public override string ToString()
+        {
+            return this;
+        }
+    }
+}
